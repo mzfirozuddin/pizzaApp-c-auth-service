a3ec002c56d25bbc7bbe3d6205dc1666cac7049c
mzfirozuddin <mz.firozuddin@gmail.com>
2023-12-13 18:05:13 +0530

    test lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 function login(username: string): string {
-  // console.log(username);
+    // console.log(username);
 
-  const user = {
-    name: "firozuddin"
-  };const name = user.name;
-  return username + name;
+    const user = {
+        name: "firozuddin",
+    };
+    const name = user.name;
+    return username + name;
 }
 
 login("firoz");
