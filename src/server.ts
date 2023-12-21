3337a75dff148368c46f61ed262eab40866dd221
mzfirozuddin <mz.firozuddin@gmail.com>
2023-12-21 12:08:15 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 (() => {
     const PORT = Config.PORT;
     try {
         app.listen(PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`Server is listening on port ${PORT}`);
+            logger.info(`Server is listening on port ${PORT}`);
         });
     } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log("Error in server-listen", error);
-        process.exit(1);
+        if (error instanceof Error) {
+            logger.error(error.message);
+
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 })();
